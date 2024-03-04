e5bdae3e57ea3a044a3ea86ddbeaf817954f01ea
francM13 <matteisfrancesco@gmail.com>
2024-03-04 23:11:18 +0100

    Updated mesh loss function

--- src/data/mesh_loss_function.py
+++ src/data/mesh_loss_function.py
@@ -4,8 +4,8 @@ from src.features.face_landmark_alignment import face_landmark_alignment
 from src.visualization.visualize_mesh import visualize_mesh
 
 
-def mesh_loss_function(mesh:o3d.geometry.TriangleMesh,flame_landmark:o3d.geometry.PointCloud,landmark:o3d.geometry.PointCloud,visualize:bool = False) -> float:
-    """	
+def mesh_loss_function(mesh:o3d.geometry.TriangleMesh,flame_landmark:o3d.geometry.PointCloud,landmark:o3d.geometry.PointCloud,visualize:bool = False):
+    """
     Calculate the loss function for the given mesh and landmarks.
      
 	Parameters:
@@ -20,15 +20,23 @@ def mesh_loss_function(mesh:o3d.geometry.TriangleMesh,flame_landmark:o3d.geometr
     # Align mesh and landmark
     face_landmark_alignment(flame_landmark,landmark)
 
-    # Convert mesh to pointcloud
-    mesh_pountcloud = o3d.geometry.PointCloud()
-    mesh_pountcloud.points = mesh.vertices     
-
-    # Calculate distances
-    distances = o3d.geometry.PointCloud.compute_point_cloud_distance(mesh_pountcloud,landmark)
-    distances=np.array(distances)
-
+    # Convert mesh and landmark to numpy arrays
+    Mesh_Vertex=np.array(mesh.vertices)
+    landmarks_Vertex=np.array(landmark.points)
+    
+    # Calculate distances between mesh vertices and landmarks
+    distances = np.linalg.norm(Mesh_Vertex[:, None] - landmarks_Vertex[None, :], axis=2)
+    
+    # Find the closest landmark for each mesh vertex
+    closest_indices = np.argmin(distances, axis=0)
+    
+    # Calculate the distances to the closest landmarks
+    closest_distances = distances[closest_indices, range(len(landmarks_Vertex))]
+    
+    # Visualize the mesh and landmarks if required
     if(visualize):
         visualize_mesh([mesh,landmark])
-
-    return distances.mean()
+    
+    # Return the mean of the closest distances
+    
+    return closest_distances.mean()#+closest_distances.max()
