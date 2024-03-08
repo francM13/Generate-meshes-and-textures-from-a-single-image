7f6cbe8cac5f7b3078825a3cd239e4633f7c3fd9
francM13 <matteisfrancesco@gmail.com>
2024-03-08 21:51:59 +0100

    Updated flame landmark alignment

--- src/features/face_landmark_alignment.py
+++ src/features/face_landmark_alignment.py
@@ -18,6 +18,21 @@ def face_landmark_alignment(flame_landmark: o3d.geometry.PointCloud, landmark: o
     flame_target = np.array([36,45,30,48,54])
     landmark_target = np.array([33,263,4,78,308])
 
+    #Center
+    center=flame_landmark.points[flame_target[2]]
+
+    # Translation
+    translation_vector = flame_landmark.points[flame_target[2]] - landmark.points[landmark_target[2]]
+    landmark.translate(translation_vector)
+
+    #Scale
+    distance_1 = np.linalg.norm(flame_landmark.points[flame_target[0]] - flame_landmark.points[flame_target[1]])
+    distance_2 = np.linalg.norm(landmark.points[landmark_target[0]] - landmark.points[landmark_target[1]])
+
+    scale_factor = distance_1 / distance_2
+    landmark.scale(scale_factor, center=center)
+
+
     def Loss_alignment(x):
         """
         A function to calculate the loss for alignment.
@@ -30,23 +45,16 @@ def face_landmark_alignment(flame_landmark: o3d.geometry.PointCloud, landmark: o
         """
         # Copy the landmark
         landmark_copy=copy.deepcopy(landmark)
-        x=x.reshape(3,3)
         
         # Rotation
-        rotate_x=o3d.geometry.get_rotation_matrix_from_xyz([x[0,0],0,0])
-        rotate_y=o3d.geometry.get_rotation_matrix_from_xyz([0,x[0,1],0])
-        rotate_z=o3d.geometry.get_rotation_matrix_from_xyz([0,0,x[0,2]])
-
-        # Translation and Scale
-        translate=x[1,:3]
-        scale=x[2,0]
+        rotate_x=o3d.geometry.get_rotation_matrix_from_xyz([x[0],0,0])
+        rotate_y=o3d.geometry.get_rotation_matrix_from_xyz([0,x[1],0])
+        rotate_z=o3d.geometry.get_rotation_matrix_from_xyz([0,0,x[2]])
 
         # Alignment
-        landmark_copy.scale(scale,center=(0,0,0))
-        landmark_copy.translate(translate.reshape(-1))
-        landmark_copy.rotate(rotate_x)
-        landmark_copy.rotate(rotate_y)
-        landmark_copy.rotate(rotate_z)
+        landmark_copy.rotate(rotate_x,center=center)
+        landmark_copy.rotate(rotate_y,center=center)
+        landmark_copy.rotate(rotate_z,center=center)
         
         # Calculate Loss
         Loss=np.sum([np.linalg.norm(landmark_copy.points[landmark_target[0]]-flame_landmark.points[flame_target[0]]),
@@ -57,28 +65,22 @@ def face_landmark_alignment(flame_landmark: o3d.geometry.PointCloud, landmark: o
         return Loss
     
     # Initial guess
-    x0 = np.zeros([3,3]).reshape(-1)  
+    x0 = np.zeros(3).reshape(-1)  
 
     # Optimize
     res = minimize(Loss_alignment, x0, tol=0.000001)
 
     # Result
     optimal_x = res.x
-    optimal_x=optimal_x.reshape(3,3)
 
     # Get Alignment parameters
-    rotate_x=o3d.geometry.get_rotation_matrix_from_xyz([optimal_x[0,0],0,0])
-    rotate_y=o3d.geometry.get_rotation_matrix_from_xyz([0,optimal_x[0,1],0])
-    rotate_z=o3d.geometry.get_rotation_matrix_from_xyz([0,0,optimal_x[0,2]])
-    translate=optimal_x[1,:3]
-    scale=optimal_x[2,0]
+    rotate_x=o3d.geometry.get_rotation_matrix_from_xyz([optimal_x[0],0,0])
+    rotate_y=o3d.geometry.get_rotation_matrix_from_xyz([0,optimal_x[1],0])
+    rotate_z=o3d.geometry.get_rotation_matrix_from_xyz([0,0,optimal_x[2]])
 
     # Align
-    # landmark=copy.deepcopy(landmark)
-    landmark.scale(scale,center=(0,0,0))
-    landmark.translate(translate.reshape(-1))
-    landmark.rotate(rotate_x)
-    landmark.rotate(rotate_y)
-    landmark.rotate(rotate_z)
+    landmark.rotate(rotate_x,center=center)
+    landmark.rotate(rotate_y,center=center)
+    landmark.rotate(rotate_z,center=center)
 
     return
